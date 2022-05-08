2378bd618c7b95a7b6d1ad43734ae3fef2d80fa6
meriem <meriembenismail@insat.u-carthage.tn>
2022-05-08 00:40:45 -0700

    env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TerrainModule } from './terrains/terrain.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://Trena:Trena123@cluster0.yr6zz.mongodb.net/trena',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
     TerrainModule,
   ],
   controllers: [AppController],
